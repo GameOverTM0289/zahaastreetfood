e90f7c4900b2d9382f8323c081745de6a353d787
leloaurel <50328347+GameOverTM0289@users.noreply.github.com>
2025-04-14 18:37:34 +0200

    Update language.js

--- middlewares/language.js
+++ middlewares/language.js
@@ -2,8 +2,7 @@
  * middlewares/language.js
  ************************************************/
 exports.forceAlbanianDefault = (req, res, next) => {
-  // If the user hasn't chosen a language (no cookie),
-  // we set the locale to 'sq' manually.
+
   if (!req.cookies.lang) {
     req.setLocale("en");
   }
